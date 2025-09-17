489793f84c9f7c7045915378fdfdf2cd11f2cafa
Scott <scottle220044@gmail.com>
2025-09-16 18:35:57 -0700

    completed fitting grid structure of divs

--- javascript.js
+++ javascript.js
@@ -3,8 +3,10 @@ const square = 4
 
 for (let i = 0; i < square; ++i){
     const outer = document.createElement("div");
+    outer.className = "line";
     for (let k = 0; k < square; ++k){
         const div = document.createElement("div");
+        div.className = "boxes";
         div.textContent = i * square + k;
         div.setAttribute("style","border-color: black;");
         outer.appendChild(div);
