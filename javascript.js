9f4364ab34355584d6fd41e9034e3b1c2e75e037
Scott <scottle220044@gmail.com>
2025-09-16 19:04:10 -0700

    implemented reset button of the Etch board

--- javascript.js
+++ javascript.js
@@ -1,22 +1,42 @@
-const container = document.querySelector("#container");
-const square = 16
 
-for (let i = 0; i < square; ++i){
-    const outer = document.createElement("div");
-    outer.className = "line";
-    for (let k = 0; k < square; ++k){
-        const div = document.createElement("div");
-        div.className = "boxes";
-        div.textContent = i * square + k;
-        div.setAttribute("style","border-color: black;");
-        outer.appendChild(div);
+const button = document.querySelector("#newEtch");
+button.addEventListener("click", ()=>{
+    let input = prompt("Enter amount of width and height blocks (Max: 100)");
+    // const deleteAllBoxes = document.querySelectorAll(".boxes");
+    // deleteAllBoxes.forEach( box => {
+    //     box.remove();
+    // });
+    
+    const container = document.querySelector("#container");
+    container.innerHTML = "";
+
+    createEtch(input);
+});
+
+
+
+function createEtch(square){
+    for (let i = 0; i < square; ++i){
+        const outer = document.createElement("div");
+        outer.className = "line";
+        for (let k = 0; k < square; ++k){
+            const div = document.createElement("div");
+            div.className = "boxes";
+            div.textContent = i * square + k;
+            div.setAttribute("style","border-color: black;");
+            outer.appendChild(div);
+        }
+        container.appendChild(outer);
     }
-    container.appendChild(outer);
+
+    const allBoxes = document.querySelectorAll(".boxes");
+    allBoxes.forEach(box => {
+        box.addEventListener("mouseover",() =>{
+            box.style.background = "black";
+        });
+    });
+
 }
 
-const allBoxes = document.querySelectorAll(".boxes");
-allBoxes.forEach(box => {
-    box.addEventListener("mouseover",() =>{
-        box.style.background = "black";
-    });
-});
+
+
